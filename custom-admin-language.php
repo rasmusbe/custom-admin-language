b4c43f3c37a4d397c8d5e71caa461a577d2fb269
Rasmus Bengtsson <rasmus@isotop.se>
2015-04-14 10:40:18 +0200

    Updated codestyle

--- custom-admin-language.php
+++ custom-admin-language.php
@@ -6,20 +6,20 @@ Version: 2.0
 
 class Custom_Admin_Language {
   function __construct() {
-    add_filter('locale', array(&$this, 'set_language'), 1);
+    add_filter( 'locale', array( &$this, 'set_language' ), 1 );
   }
 
-  function set_language($lang) {
-    if (!is_admin() ||
-      (!empty($_GET['page']) && $_GET['page'] == 'wc-status') ||
-      (!empty($_GET['page']) && $_GET['page'] == 'wc-settings' && !empty($_GET['install_woocommerce_pages'])) ||
-      (!empty($_SERVER['PHP_SELF']) && $_SERVER['PHP_SELF'] == '/wp-admin/options-general.php') && empty($_GET['page']) ||
-      (!empty($_SERVER['PHP_SELF']) && $_SERVER['PHP_SELF'] == '/wp-admin/options-permalink.php') && empty($_GET['page'])
+  function set_language( $lang ) {
+    if ( !is_admin() ||
+      ( !empty( $_GET['page'] ) && $_GET['page'] == 'wc-status' ) ||
+      ( !empty( $_GET['page'] ) && $_GET['page'] == 'wc-settings' && !empty( $_GET['install_woocommerce_pages'] ) ) ||
+      ( !empty( $_SERVER['PHP_SELF'] ) && $_SERVER['PHP_SELF'] == '/wp-admin/options-general.php' ) && empty( $_GET['page'] ) ||
+      ( !empty( $_SERVER['PHP_SELF'] ) && $_SERVER['PHP_SELF'] == '/wp-admin/options-permalink.php' ) && empty( $_GET['page'] )
     ) {
       return $lang;
     }
 
-    return defined('ADMIN_LANG') ? ADMIN_LANG : 'sv_SE';
+    return defined( 'ADMIN_LANG' ) ? ADMIN_LANG : 'sv_SE';
   }
 }
 new Custom_Admin_Language;
