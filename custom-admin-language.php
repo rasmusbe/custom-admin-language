ee5ae55ed125d6f4a7dba57a3ff2606d3581e270
Rasmus <rasmusbe@users.noreply.github.com>
2015-10-05 14:27:42 +0200

    Fix some code descriptions

--- custom-admin-language.php
+++ custom-admin-language.php
@@ -11,14 +11,14 @@
 class Custom_Admin_Language {
 
 	/**
-	 * Determine if return url should be returned or not.
+	 * Determine if site (original) or custom language should be returned.
 	 *
 	 * @var bool
 	 */
 	protected $return_original = false;
 
 	/**
-	 * The constructor.
+	 * The constructor. Adds filters to modify language.
 	 */
 	public function __construct() {
 		if ( is_admin() && ! is_network_admin() ) {
@@ -30,7 +30,8 @@ class Custom_Admin_Language {
 	}
 
 	/**
-	 * Set language.
+	 * Set admin language.
+	 * Defaults to sv_SE if ADMIN_LANG is undefined.
 	 *
 	 * @param string $lang
 	 */
@@ -43,7 +44,9 @@ class Custom_Admin_Language {
 	}
 
 	/**
-	 * Fix WooCommerce so it can handle custom admin language.
+	 * Fix for WooCommerce so it can handle custom admin language without breaking non-admin pages.
+	 * 
+	 * @return bool   If true set_language will use site language, otherwise custom language.
 	 */
 	public function fix_woocommerce() {
 		// Check if WooCommerce is active.
@@ -76,7 +79,7 @@ class Custom_Admin_Language {
 			return true;
 		}
 
-		// Set WooCommerce permalinks if not set
+		// Set WooCommerce permalinks to site language if not set. Will otherwise break permalinks.
 		$this->return_original = true;
 		$permalinks = get_option( 'woocommerce_permalinks' );
 
@@ -110,13 +113,15 @@ class Custom_Admin_Language {
 
 		$this->return_original = false;
 
+		// Reload WooCommerce textdomain
 		if ( $was_loaded ) {
 			load_plugin_textdomain( 'woocommerce', false, WP_PLUGIN_DIR . '/woocommerce/i18n/languages' );
 		}
 	}
 
 	/**
-	 * Add settings fields for language fixes.
+	 * Add settings fields to fix get_locale in general settings.
+	 * Quite ugly fix, but WP have no actions or filters to use here.
 	 */
 	public function site_language_fix() {
 		add_settings_field( 'site_language_fix_1', '', [ $this, 'site_language_fix_1' ], 'general', 'default' );
@@ -124,7 +129,7 @@ class Custom_Admin_Language {
 	}
 
 	/**
-	 * Site language fix 1.
+	 * Site language fix 1. Runs before language dropdown.
 	 * Sets the `return_original` to true.
 	 */
 	public function site_language_fix_1() {
@@ -133,7 +138,7 @@ class Custom_Admin_Language {
 	}
 
 	/**
-	 * Site language fix 2.
+	 * Site language fix 2. Runs after language dropdown.
 	 * Sets the `return_original` to false.
 	 */
 	public function site_language_fix_2() {
@@ -142,7 +147,7 @@ class Custom_Admin_Language {
 	}
 
 	/**
-	 * Fix WooCommerce language buttons.
+	 * Fix WooCommerce language buttons if custom language is english.
 	 *
 	 * @param  array $tools
 	 *
